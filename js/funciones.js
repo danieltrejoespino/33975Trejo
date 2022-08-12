af79d72fb156d2cb4b608748f7e4d97088019ed6
DanielTrejom <daniel.trejo.espino@gmail.com>
2022-08-11 21:32:25 -0500

    Cambios clase 8

--- js/funciones.js
+++ js/funciones.js
@@ -1,42 +1,50 @@
 function agregaGastos() {
-    let adTd=document.getElementById("miTabla");
+    const adTd=document.getElementById("miTabla");
     adTd.insertRow(-1).innerHTML=
-    "<tr><td><input type='text' placeholder='Nombre del gasto'></td><td><input class='costoG' type='text' placeholder='Costo' ></td><td><button type='button' class='btn btn-danger'>D</button></tr></tr>";
+    "<tr><td><input type='text' placeholder='Nombre del gasto'></td><td><input class='costoG' type='text' placeholder='Costo' ></td><td><button type='button' class='btn btn-danger' onclick='deleteGasto()'>D</button></tr></tr>";
 }
-
-
-
-function calcularGastos() {      
-    document.getElementsByClassName("totalmes").innerHTML="";
-    // var mes=document.getElementById("meses").value;  
+function calcularGastos() {   
+    document.getElementById("totalmes").innerHTML="";
+    document.getElementById("totalmeses").innerHTML=""; 
+    const mes=document.getElementById("meses").value;  
     const gastos = document.getElementsByClassName("costoG");  
     const total=[];
     let sum = 0;
-
-    // if (mes.length==0) {
-    //     alert("Debes ingresar los meses a calcular");
-    // }else{
-
+ 
+    if (mes.length==0) {
+        alert("Debes ingresar minimo un mes para calcular");
+    }else{
         for (let i = 0; i <gastos.length; i++) {
             let gastos =document.getElementsByClassName("costoG")[i].value;           
-            total.push(gastos);
-            
+            total.push(gastos);            
             // console.log(gastos);
         }
         console.log(total);
          //  toal.forEach(element => console.log(element));
         for (let i = 0; i < total.length; i++) {
             sum += parseFloat(total[i]);
-        }    
-        document.getElementById("totalmes").innerHTML+="<h5>"+"Lo que ahorras en el mes es un total de: $"+sum+"</h5>";
-
-        // for (let i = 1; i <=mes; i++) {
-        //     totalMes+=sum*parseFloat(mes);
-        //     document.getElementById("totalmes").innerHTML+="Para el mes "+i+" ahorras $"+totalMes+"<br>";
+        }
+        if (mes>1) {
+            let sumMes=sum*mes;
+            document.getElementById("totalmeses").innerHTML+="<h5>"+"Lo que gastas en "+mes+" meses es un total de= $"+sumMes+"</h5>";
+            console.log(sum)
+            console.log(mes)
             
-        // }     
-   // } 
+        }else{
+            document.getElementById("totalmes").innerHTML+="<h5>"+"Lo que gastas en un mes es un total de: $"+sum+"</h5>"; 
+        }  
+    }
+
+         
 }
+function deleteGasto() {
+//alert("hola")      
+} 
 
+document.getElementById("limpiarGastos").addEventListener("click", function() { 
+    document.getElementById("miTabla").innerHTML="";
+    document.getElementById("totalmes").innerHTML="";
+    document.getElementById("meses").value="";
 
-
+    // alert("hola")
+})
